6e524e112cc209e1f8106b2f080b6949d916f72e
Фёдор Колов <fedor.kolov@leadexsystems.ru>
2015-04-03 14:28:48 +0500

    [3] Обновлена таблица для хранения категорий.

--- backup.sql
+++ backup.sql
@@ -1,3 +1,8 @@
 CREATE TABLE films(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);
-CREATE TABLE category(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,id_parent INTEGER);
 CREATE TABLE category_images(id INTEGER PRIMARY KEY AUTOINCREMENT, value BLOB);
+CREATE TABLE category_films(id_films INTEGER, id_category INTEGER,FOREIGN KEY(id_films) REFERENCES films(id), FOREIGN KEY(id_category) REFERENCES category(id));
+CREATE TABLE [category] (
+  [id] INTEGER PRIMARY KEY AUTOINCREMENT, 
+  [name] TEXT, 
+  [id_image] INTEGER REFERENCES [category_images]([id]), 
+  [id_parent] INTEGER);
