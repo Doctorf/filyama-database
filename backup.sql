4daab9ef2310b0fcf110b465884473a927cddd86
Фёдор Колов <fedor.kolov@leadexsystems.ru>
2015-06-11 17:00:05 +0500

    Добавление таблицы directed

--- backup.sql
+++ backup.sql
@@ -5,7 +5,15 @@ CREATE TABLE [category] (
   [id_image] INTEGER REFERENCES [category_images]([id]), 
   [id_parent] INTEGER);
 CREATE TABLE category_images(id INTEGER PRIMARY KEY AUTOINCREMENT, value BLOB);
+CREATE TABLE [directed] (
+  [id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+  [full_name] VARCHAR(255));
 CREATE TABLE [films] (
   [id] INTEGER PRIMARY KEY AUTOINCREMENT, 
-  [name] TEXT, 
-  [existCover] BOOLEAN DEFAULT false);
+  [existCover] BOOLEAN DEFAULT false, 
+  [name_orig] TEXT, 
+  [name_rus] TEXT, 
+  [id_directed] INTEGER REFERENCES [directed]([id]), 
+  [date_world] TIMESTAMP, 
+  [date_rus] TIMESTAMP, 
+  [path] VARCHAR(512));
