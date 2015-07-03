765b052d2dc7b958e75d1709b469a5545c54728c
Фёдор Колов <fedor.kolov@leadexsystems.ru>
2015-07-03 15:29:25 +0500

    [6] Добавление таблицы расширений файлов
    1) Добавление таблицы расширений файлов
    2) Обложа хранится в бинарных файлах
    3) К параметру фильма добавлена главная обложка.

--- backup.sql
+++ backup.sql
@@ -8,11 +8,25 @@ CREATE TABLE category_images(id INTEGER PRIMARY KEY AUTOINCREMENT, value BLOB);
 CREATE TABLE [directed] (
   [id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
   [full_name] VARCHAR(255));
+CREATE TABLE [file_extensions] (
+  [id] INTEGER PRIMARY KEY AUTOINCREMENT, 
+  [Name] varcHAR(255), 
+  [filter] varchar(255), 
+  [foto] BOOLEAN DEFAULT false);
+CREATE TABLE [binary_data_films] (
+  [id_films] INTEGER NOT NULL REFERENCES [films]([id]), 
+  [id_binary_data] INTEGER NOT NULL REFERENCES [binary_data]([id]));
+CREATE TABLE [binary_data] (
+  [id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+  [url] VARCHAR(255), 
+  [is_thumbnails] BOOLEAN DEFAULT false, 
+  [is_frame] BOOLEAN DEFAULT false, 
+  [is_cover] BOOLEAN DEFAULT false);
 CREATE TABLE [films] (
   [id] INTEGER PRIMARY KEY AUTOINCREMENT, 
-  [existCover] BOOLEAN DEFAULT false, 
   [name_orig] TEXT, 
   [name_rus] TEXT, 
+  [id_cover] INTEGER REFERENCES [binary_data]([id]), 
   [id_directed] INTEGER REFERENCES [directed]([id]), 
   [date_world] TIMESTAMP, 
   [date_rus] TIMESTAMP, 
